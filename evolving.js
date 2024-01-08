a86ab2578188a2d47547f8ab186ce231930662f4
whoisluci <lucmov99@gmail.com>
2024-01-08 15:41:22 +0100

    Updated entire website

--- evolving.js
+++ evolving.js
@@ -30,9 +30,3 @@ function closeModal(modalId) {
     modal.style.display = 'none';
 }
 
-// Close modal if the user clicks outside of it
-window.onclick = function (event) {
-    if (event.target.classList.contains('modal')) {
-        event.target.style.display = 'none';
-    }
-};
